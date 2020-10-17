33874df784d5c0d5211e75c67628ff4723cebfee
ylj <1025230700@qq.com>
2020-10-17 19:00:13 +0800

    V1.1

---
+++ src/components/resource-usage/resource-service.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div class="content">
+      <el-input
+        style="height: 30px；width:50%"
+        placeholder="请输入搜索内容"
+        prefix-icon="el-icon-search"
+        v-model="input"
+      >
+      </el-input>
+      <el-row>
+
+      </el-row>
+      <el-row>
+        
+      </el-row>
+    </div>
+  </div>
+</template>
+<style>
+.el-input__inner {
+  height: 30px;
+  line-height: 30px;
+  width: 95%;
+  margin-top: 10px;
+}
+.el-input__icon {
+  line-height: 30px;
+  margin-top: 10px;
+  margin-left: 35px;
+}
+.container {
+  width: 100%;
+  height: 100%;
+}
+.content {
+  height: 80%;
+  background-color: gray;
+}
+el-input {
+  width: 95%;
+  margin-top: 10px;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: "1",
+      activeIndex2: "1",
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
---
+++ src/components/resource-usage/sidebar.vue
@@ -0,0 +1,64 @@
+<template>
+  <!-- sidebar -->
+  <div style="width: 10%; height: 600px">
+    <div class="title">资源使用者/应用开发者</div>
+    <el-menu
+      default-active="1"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#2e324c"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+    >
+      <el-menu-item index="1">
+        <span>资源服务</span>
+        <i class="el-icon-arrow-right"></i>
+      </el-menu-item>
+
+      <el-menu-item index="2">
+        <span>应用服务</span>
+        <i class="el-icon-arrow-right"></i>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <span>最佳实践</span>
+        <i class="el-icon-arrow-right"></i>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+<style scoped>
+.title {
+  width: 100%;
+  height: 35px;
+  background-color: #2e324c;
+  color: white;
+  border: 1px solid gray;
+  line-height: 35px;
+  font-size: 13px;
+}
+ul {
+  border: 1px solid gray;
+  width: 100%;
+  height: 80%;
+  text-align: left;
+}
+i {
+    margin-left: 45%;
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: "1",
+      activeIndex2: "1",
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
