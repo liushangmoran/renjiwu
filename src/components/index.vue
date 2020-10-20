fa54a726264273b49c7e302d43870e80c90def72
liushangmoran <49028575+liushangmoran@users.noreply.github.com>
2020-10-20 10:05:36 +0800

    V1.9

---
+++ src/components/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-container style="height:100%">
+      <sidebar></sidebar>
+      <el-container direction="vertical">
+        <navheader></navheader>
+        <el-main>
+         <physicalDevice></physicalDevice>
+        </el-main>
+      </el-container>
+    </el-container>
+</template>
+<script>
+import navheader from "./header";
+import physicalDevice from "./resource-provider/physical-device";
+import sidebar from "./resource-provider/sidebar";
+export default {
+  components:{
+    navheader,
+    physicalDevice,
+    sidebar
+  }
+};
+</script>
+
+<style>
+  body{
+    margin: 0px;
+  }
+</style>
+
